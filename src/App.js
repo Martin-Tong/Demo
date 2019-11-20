b6ed3a7442087465f94a5fbcd4bad396b91fd3ff
trk19931112 <1933981377@qq.com>
2019-11-20 13:15:04 +0800

    'start'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Block(props) {
+  return <p>{props.value}</p>
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: ''
+    }
+  }
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value
+    })
+  }
+  render() {
+    return (
+      <div>
+        <Block value={this.state.value} />
+        <label>输入框：
+          <input type='text' value={this.state.value} onChange={this.handleChange} />
+        </label>
+      </div>
+    )
+  };
 }
 
 export default App;
