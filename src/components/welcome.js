495fd8a91634b26602f9f4209e43303067ba30c6
trk19931112 <1933981377@qq.com>
2019-12-27 12:09:38 +0800

    '2019.12.27-1'

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,52 +1,134 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 import style from '../styles/countdown.module.css'
+import * as PIXI from 'pixi.js-legacy'
 
-import numberSix from '../number-six.png'
-import numberTen from '../number-ten.png'
+let Sprite = PIXI.Sprite,
+    Application = PIXI.Application,
+    Text = PIXI.Text,
+    container,
+    keyup = keybordMove('ArrowRight')
 
-let imageOne = document.createElement('img')
-imageOne.src = numberSix
-let imageTwo = document.createElement('img')
-imageTwo.src = numberTen
-
-function drawImage(ctx, h, m, s) {
-    if (s > 0) {
-        let sta = s.toString().split('')//秒数数组
-        let s1 = +sta[1]
-        let s2 = +sta[0]
-        ctx.drawImage(imageTwo, 0, 115*s1, 90, 90, 400, 0, 90, 90)
-        ctx.drawImage(imageOne, 0, 115*s2, 90, 90, 350, 0, 90, 90)
-        let va = setInterval(() => {
-            --s
-            drawImage(ctx, h, m, s)
-        }, 1000)
+function keybordMove(keycode) {
+    let key = {}
+    key.value = keycode
+    key.isUp = true
+    key.isDown = false
+    key.press = undefined
+    key.release = undefined
+    key.downHandler = event => {
+        if (event.key === key.value) {
+            if (key.isUp && key.press) key.press()
+            key.isDown = true
+            key.isUp = false
+            event.preventDefault()
+        }
     }
+    key.upHandler = event => {
+        if (event.key === key.value) {
+            if (key.isDown && key.release) key.release()
+            key.isUp = true
+            key.isDown = false
+            event.preventDefault()
+        }
+    }
+    const downListener = key.downHandler.bind(key)
+    const upListener = key.upHandler.bind(key)
+    window.addEventListener('keydown', downListener, false)
+    window.addEventListener("keyup", upListener, false)
+    key.unSubscribe = function() {
+        window.removeEventListener("keyup", upListener, false)
+        window.removeEventListener("keydown", downListener, false)
+    }
+    return key
+}
+function countdownInitial(time) {
+    let type = 'WebGL',
+        app = new Application({width: 560, height: 100, transparent: true}),
+        code = {},
+        countdownTime = time ? time : '000-00-00' //开始倒计时的时间，后端返回
+         
+    container = document.querySelector("#countdown")
 
+    if (!PIXI.utils.isWebGLSupported()) {
+        type = 'canvas'
+    }
+    PIXI.utils.sayHello(type)
+    Object.assign(app.renderer.view.style, {
+        position: 'relative',
+        top: '50%',
+        left: '50%',
+        transform: 'translateX(-50%) translateY(-50%)'
+    })
+    container.appendChild(app.view)
+    let text1 = new Text(':'),
+        text2 = new Text(':')
+    text1.style = {fill: 'white'}
+    text2.style = {fill: 'white'}
+    text1.position.set(220,50)
+    text2.position.set(390,50)
+    app.stage.addChild(text1, text2)
+    //y:112
+    app.loader.add(['number-six.png', 'number-ten.png']).load((loader, resource) => {
+        let list = countdownTime.split('-').join('').split(''),
+            length = list.length
+        for (let i = 0; i < length; i++) {
+            if (i === (length - 2) || i === (length - 4)) {
+                code[i] = new Sprite(app.loader.resources['number-six.png'].texture)
+            } else {
+                code[i] = new Sprite(app.loader.resources['number-ten.png'].texture)
+            }               
+            code[i].x = i*80
+            code[i].y = -112*list[i]
+            app.stage.addChild(code[i])
+                
+        }                                   
+    })
+    setInterval(() => {
+        code[6].y -= 111
+    }, 1000);
 }
 
-export default function Welcome() {
-    const [show, setShow] = useState(1)
-    useEffect(() => {
-        let canvas = document.getElementById('countdown-canvas');
-        let context = canvas.getContext('2d')
-        //115
-        //drawImage(context, 60, 60, 25)
-    })
-    let styles = {
-        opacity: show,
-        transform: 'scale('+show+')'
+export default class Welcome extends React.Component {
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this)
+        this.state = {
+            show: 1
+        };
     }
-    function toggle() {
-       show === 0 ? setShow(1) : setShow(0)
+    componentDidMount() {
+        countdownInitial()
     }
-    return (
-        <div>
-            <img src='/number-six.png' style={{display:'none'}} />
-            <div id="countdown" className={style.welcomeCanvas} style={styles}>
-                <canvas id="countdown-canvas" className={style.countdownCanvas} width="450" height="100"></canvas>
-                <div className={style.close} onClick={toggle}></div>
+    componentDidUpdate(){
+        alert('ComponentDidUpdate')
+    }
+    componentWillUnmount() {
+        container.removeChild(container.lastChild)
+        alert('componentWillUnmount')
+    }
+    toggle() {
+       this.state.show === 0 ? this.setState({show: 1}) : this.setState({show: 0})
+    }
+    render() {
+        const styles={
+            opacity: this.state.show,
+            transform: 'scale('+this.state.show+')'
+        }
+        return (
+            <div>         
+                <div id="countdown" className={style.welcomeCanvas} style={styles}>
+                    <div className={style.description}>
+                        <div style={{fontSize: 'x-large'}}>countdown for what</div>
+                        <div className={style.hms}>
+                            <span className={style.h}>Hours</span>
+                            <span className={style.m}>Minutes</span>
+                            <span className={style.s}>Seconds</span>
+                        </div>
+                    </div>
+                    <div className={style.close} onClick={this.toggle}></div>
+                </div>
+                <div className={style.showControl} onClick={this.toggle}></div>
             </div>
-            <div className={style.showControl} onClick={toggle}></div>
-        </div>
-    )
+        )
+    }  
 }
